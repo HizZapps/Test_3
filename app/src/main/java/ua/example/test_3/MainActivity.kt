a092a438ddc2c649f9c8f87d18ec36038d49911d
user <HizZapps>
2022-10-25 17:22:50 +0300

    Start Work

--- app/src/main/java/ua/example/test_3/MainActivity.kt
+++ app/src/main/java/ua/example/test_3/MainActivity.kt
@@ -2,16 +2,23 @@ package ua.example.test_3
 
 /**
  * 25.10.22
- * From work
+ * From work 25.10.22
  */
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity : AppCompatActivity() {
+
+    val  count = 10;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(count  * 2)
 
     }
 }
